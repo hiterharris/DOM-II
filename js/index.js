a99c559d0414888907922cdc09bda022297ad1ac
Hiter Harris <hiterharris@comcast.net>
2019-12-02 21:12:22 -0500

    first 3 event listeners for tast 3 complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-// Your code goes here
+// MOUSEOVER
+document.querySelector('.logo-heading').addEventListener("mouseover", mouseOver);
+document.querySelector('.logo-heading').addEventListener("mouseout", mouseOut);
+function mouseOver() {
+    document.querySelector('.logo-heading').style.borderBottom = '2px solid black';
+  }
+function mouseOut() {
+document.querySelector('.logo-heading').style.borderBottom = '0px solid black';  
+}
+
+// KEYDOWN
+document.querySelector('body').addEventListener("keydown", keydown);
+function keydown() {
+  document.querySelector('body').style.backgroundColor = "lightgrey";
+}
+
+// WHEEL
+document.querySelector('.intro p').addEventListener("wheel", wheel);
+function wheel() {
+  this.style.fontSize = "24px";
+}
