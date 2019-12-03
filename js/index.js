9f8657d67855b03bb8e472389c7f8576401bf785
Hiter Harris <hiterharris@comcast.net>
2019-12-03 14:14:49 -0500

    completed Tast 2

--- js/index.js
+++ js/index.js
@@ -1,21 +1,56 @@
+// CLICK
+document.querySelectorAll('p').forEach(item => {
+  item.addEventListener('click', () => {
+    item.style.color = 'blue';
+  });
+});
+
 // MOUSEOVER
-document.querySelector('.logo-heading').addEventListener("mouseover", mouseOver);
-document.querySelector('.logo-heading').addEventListener("mouseout", mouseOut);
-function mouseOver() {
-    document.querySelector('.logo-heading').style.borderBottom = '2px solid black';
-  }
-function mouseOut() {
-document.querySelector('.logo-heading').style.borderBottom = '0px solid black';  
-}
+const logo = document.querySelector('.logo-heading');
+logo.addEventListener("mouseover", () => {
+  logo.style.borderBottom = '2px solid black';
+});
+
+// MOUSEOUT
+logo.addEventListener("mouseout", () => {
+  logo.style.borderBottom = '0px solid black';
+});
 
 // KEYDOWN
-document.querySelector('body').addEventListener("keydown", keydown);
-function keydown() {
-  document.querySelector('body').style.backgroundColor = "lightgrey";
-}
+const body = document.querySelector('body');
+body.addEventListener("keydown", () => {
+  body.style.backgroundColor = "lightgrey";
+});
 
 // WHEEL
-document.querySelector('.intro p').addEventListener("wheel", wheel);
+document.querySelector('.intro').addEventListener('wheel', (wheel));
 function wheel() {
-  this.style.fontSize = "24px";
+  this.style.color = 'blue';
 }
+
+// DOUBLECLICK
+const newImg = document.querySelector('.intro img');
+newImg.addEventListener('dblclick', () => {
+  newImg.src= 'img/bus2.png';
+});
+
+// RESIZE
+window.addEventListener('resize', () => {
+  newImg.src = 'img/bus2.png';
+});
+
+// DRAGSTART
+const imgContent = document.querySelector('.img-content img');
+imgContent.addEventListener('dragstart', function(event) {
+  event.target.src = 'img/bus2.png';
+}, false);
+
+// DRAGEND
+imgContent.addEventListener('dragend', function(event) {
+  event.target.src = 'img/adventure.jpg';
+}, false);
+
+// LOAD
+window.addEventListener('load', () => {
+  console.log('Page is fully loaded!');
+});
